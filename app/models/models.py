88a37e8df80e58d948a40f70206c61a861b66428
salmanbashar1009 <salmanbashar101@gmail.com>
2026-04-29 17:42:51 +0600

    feat: User Address model added

--- app/models/models.py
+++ app/models/models.py
@@ -19,3 +19,24 @@ class user(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     # Relationships
+    addresses = relationship('Address', back_populates= "user", cascade = 'all, delete-orphan')
+    
+
+
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(UUID(as_uuid=True), primary_key=True, default= uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True),ForeignKey("users.id"),nullable=False)
+    label = Column(String(50),) # e.g., Home, Work
+    street = Column(String(255), nullable=False)
+    city = Column(String(100), nullable=False)
+    state = Column(String(100), nullable=False)
+    zip_code = Column(String(20), nullable=False)
+    country = Column(String(100), nullable=False)
+    phone = Column(String(20))
+    is_default_shipping  = Column(Boolean, default=False)
+    is_default_billing = Column(Boolean, default=False)
+
+    # Relationships
+    user = relationship('User', back_populates='addresses')
